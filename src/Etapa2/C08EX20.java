fd71fbca4e51ee24f638a10b8d142231dda8ef46
Angelo Santos <angelo@bonseir.com>
2026-04-14 15:19:55 -0300

    Exercicios da aula de AED I do curso de Ciência da Computação da Dom Helder

--- src/Etapa2/C08EX20.java
+++ src/Etapa2/C08EX20.java
@@ -8,19 +8,19 @@ import java.util.Scanner;
 public class C08EX20 {
     public static void main(String[] args) {
         int numN;
-        BigInteger calc =  BigInteger.ONE;
-        BigInteger soma = BigInteger.ZERO;
+        double calc = 0, soma = 0;
 
         Scanner teclado = new Scanner(System.in);
         System.out.println("Digite o numero de repetições: ");
         numN = teclado.nextInt();
 
-        for (int i = 1; i <= numN; i++) {
-            System.out.println(calc);
-            soma = soma.add(calc); //funcao 2^n - 1 ou BigInteger.TWO.pow(numN).subtract(BigInteger.ONE);
-            calc = calc.multiply(BigInteger.TWO); //Mesmo que calc *= 2
+        for (int i = 0; i <= numN; i++) {
+            soma += calc;
+            calc = Math.pow(2,i);
+            System.out.printf("%1.0f",calc);
+            System.out.println();
         }
-        System.out.println("A soma é " +soma);
+        System.out.printf("A soma é %1.0f " ,soma);
         teclado.close();
     }
 }
